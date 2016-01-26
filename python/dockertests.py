9faf584e506be9b221d8f9b1fe18b5968913eee8
Mateusz Maciaś <mmacias@piap.pl>
2016-01-26 07:51:16 +0100

    Some clenups and fixes, added messages pairing by contend for robot information.

--- python/dockertests.py
+++ python/dockertests.py
@@ -9,49 +9,38 @@ prefix='/home/mmacias/r5cop_benchmarking/docker/'
 
 imgs=[]
 
-
 name='mother:qt'
 dockerfile = None
 path = prefix+ 'qt/'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
 name='mother:qt_ros'
 dockerfile = None
 path = prefix+ 'qt_ros'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
 name='mother:nvidia_qt'
 dockerfile = None
 path = prefix+ 'nvidia_qt/'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
 name='mother:nvidia_qt_ros'
 dockerfile = None
 path = prefix+ 'qt_ros'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
 name='mother:dep_qt'
 dockerfile = None
 path = prefix+ 'dep_qt'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
 name='mother:dep_qt_ros'
 dockerfile = None
 path = prefix+ 'dep_qt_ros'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
-
 name='mother:dds'
 dockerfile = None
 path = prefix+ 'dds'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
-
 name='mother:dep_qt_ros2'
 dockerfile = None
 path = prefix+ 'dep_qt_ros2'
 imgs.append({'Name':name,'Dockerfile':dockerfile, 'Path':path})
-
 name='mother:mother_rtrc'
 dockerfile = None
 path = prefix+ 'mother_rtrc'
@@ -111,22 +100,35 @@ containers=[
         [{'Image':'mother:mother_ros2','Command':'/tests/robot_ros2.sh','Hostname':'robot'},
             {'Image':'mother:mother_ros2','Command':'/tests/console_ros2.sh','Hostname':'console'}]
         ]
-tests=[0,1,5,10,20,30,40,60,80,90]
+
+def run_loss_tests(tests):
+    for cont in containers:
+        for test in tests:
+            cont_robot=cont[0]
+            cont_console=cont[1]
+            testname='Image: '+cont[0].get('Image')+' Test: '+ str(test) +' loss'
+            testcmd='sudo /sbin/tc qdisc replace dev {} root netem loss '+str(test)+ '%'
+            testdict={'Name':testname,'Command':testcmd}
+            c.test(cont_robot,cont_console,testdict,tcpdump=True,logs=True,testtime=240)
+            time=c.getTimeStr()
+
+def run_delay_tests(tests):
+    for cont in containers:
+        for test in tests:
+            cont_robot=cont[0]
+            cont_console=cont[1]
+            testname='Image: '+cont[0].get('Image')+' Test: '+ str(test) +' delay'
+            testcmd='sudo /sbin/tc qdisc replace dev {} root netem delay '+str(test)+ 'ms'
+            testdict={'Name':testname,'Command':testcmd}
+            c.test(cont_robot,cont_console,testdict,tcpdump=True,logs=True,testtime=240)
+            time=c.getTimeStr()
 
 
-for cont in containers:
-    for test in tests:
-        cont_robot=cont[0]
-        cont_console=cont[1]
-        testname='Image: '+cont[0].get('Image')+' Test: '+ str(test) +' loss'
-        testcmd='sudo /sbin/tc qdisc replace dev {} root netem loss '+str(test)+ '%'
-        testdict={'Name':testname,'Command':testcmd}
-        print(str(cont_robot))
-        print(str(cont_console))
-        print(str(testdict))
-        c.test(cont_robot,cont_console,testdict,tcpdump=True,logs=True,testtime=240)
-        time=c.getTimeStr()
-        #t=TestFile(time)
-        #t.plotCmdVel()
-        #t.plotRobotInformation()
-        #t.plotSave()
+
+loss_tests=[0,1,5,10,20,30,40,60,80,90]
+delay_tests=[0,10,50,100,500,800,1000]
+
+if __name__ == "__main__":
+    run_loss_tests(loss_tests)
+    run_delay_tests(delay_tests)
+
