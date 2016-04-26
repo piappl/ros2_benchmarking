ec4b6d9efea8b5742e2788baa02ba9727c762cb9
Mateusz Maciaś <mmacias@piap.pl>
2016-04-26 09:59:34 +0200

    Added more tests.

--- python/run_tests.py
+++ python/run_tests.py
@@ -7,8 +7,8 @@ from files import TestFiles
 
 
 if __name__ == "__main__":
-        test_name="loss_test"
-        plot_name="Network loss"
-        #test_name="delay_test"
-        #plot_name="Network delay"
+        #test_name="loss_test"
+        #plot_name="Network loss"
+        test_name="delay_test"
+        plot_name="Network delay"
         t=TestFiles(test_name,plot_name)
